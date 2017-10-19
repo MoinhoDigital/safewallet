ca76ea5e61ec205d4985a5a4b961a55d817a8a1f
luandro <luandro@gmail.com>
2017-10-19 14:27:22 -0200

    cleanup

--- .history/src/utils/requestShareMdAuth_20171018232318.js
+++
@@ -1,5 +0,0 @@
-export default async function (appHandle, mdPermissions) {
-  const perm = await window.safeApp.authoriseShareMd(appHandle, mdPermissions)
-  console.log('authoriseShareMd:', perm)
-  return null
-}
